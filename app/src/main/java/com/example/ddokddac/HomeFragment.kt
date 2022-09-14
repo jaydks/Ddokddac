77032921035a38dd1f870631904ceafe2e634fcb
jaydks <jjeee1313@gmail.com>
2022-09-15 03:59:00 +0900

    뷰페이저 무한, auto, 인디케이터

---
+++ app/src/main/java/com/example/ddokddac/HomeFragment.kt
@@ -0,0 +1,32 @@
+package com.example.ddokddac
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.ddokddac.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+
+        binding.search.setOnClickListener{
+            val intent = Intent(context, SearchActivity::class.java)
+            startActivity(intent)
+        }
+
+
+        return binding.root
+    }
+
+
+}
--- app/src/main/java/com/example/ddokddac/MainActivity.kt
+++ app/src/main/java/com/example/ddokddac/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.ddokddac
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.ddokddac.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        // nav
+        val navController = supportFragmentManager.findFragmentById(R.id.container_main)?.findNavController()
+        navController?.let{
+            binding.bottomnav.setupWithNavController(it)
+        }
+
     }
 }
